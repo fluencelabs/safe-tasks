20b1f669a766d9f78123eedbeb844569cb61ab48
Dima <crystalbit@mail.ru>
2024-03-27 14:32:30 +0300

    mnemonic wallet, assertion

--- src/signer-v6.ts
+++ src/signer-v6.ts
@@ -2,9 +2,14 @@ import dotenv from "dotenv";
 import { ethers6 } from "ethers6-proxied";
 
 dotenv.config();
-const { NETWORK, NODE_URL, INFURA_KEY, MNEMONIC, PK, SOLIDITY_VERSION, SOLIDITY_SETTINGS } = process.env;
+const { NODE_URL, MNEMONIC, PK } = process.env;
+
+if (!PK && !MNEMONIC) {
+    throw new Error("Please set either MNEMONIC or PK in .env");
+}
 
 export const providerv6 = new ethers6.JsonRpcProvider(NODE_URL);
-export const signerv6 = new ethers6.Wallet(PK as string, providerv6);
-
+export const signerv6 = Boolean(PK)
+  ? new ethers6.Wallet(PK!, providerv6)
+  : ethers6.Wallet.fromPhrase(MNEMONIC!, providerv6);
 
